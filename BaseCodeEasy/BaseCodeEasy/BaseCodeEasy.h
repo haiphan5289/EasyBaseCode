6e42688e8d8a8c3365d345f4d5e1c3bc56dff148
haiphan5289 <haiphan5289@gmail.com>
2022-04-08 10:59:21 +0700

    pod init

---
+++ BaseCodeEasy/BaseCodeEasy/BaseCodeEasy.h
@@ -0,0 +1,18 @@
+//
+//  BaseCodeEasy.h
+//  BaseCodeEasy
+//
+//  Created by haiphan on 07/04/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for BaseCodeEasy.
+FOUNDATION_EXPORT double BaseCodeEasyVersionNumber;
+
+//! Project version string for BaseCodeEasy.
+FOUNDATION_EXPORT const unsigned char BaseCodeEasyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BaseCodeEasy/PublicHeader.h>
+
+
